b932027469560b5aa3f8e815e0771b985f1c4007
Leandro Contreras <leandro_cont_95@hotmail.com>
2023-01-31 10:47:33 -0300

    no se que toco pero va saliendo

--- .JS/function.js
+++ .JS/function.js
@@ -6,3 +6,22 @@ function moreInfo() {
       call.style.display = 'none';
     }
   }
+
+
+  function myFunction(){
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  window.onclick = function(event){
+    if(!event.target.matches('boton')) {
+      var desplegable =
+      document.getElementsByClassName("datos");
+      var i;
+      for (i = 0; i < desplegable.length; i++) {
+        var openDropdown = desplegable [i];
+        if (openDropdown.classList.contains('show')){
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
